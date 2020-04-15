586bc6573108198b0695bda84662164d346f63d9
Tae-ho Kim <HoTaeWang@gmail.com>
2020-04-16 00:45:56 +0900

    Initial commit

---
+++ src/test/java/EarthQuake/EarthQuakeClientTest.java
@@ -0,0 +1,20 @@
+package EarthQuake;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EarthQuakeClientTest {
+
+    @Test 
+    public void testCreateCSV() {
+        EarthQuakeClient client = new EarthQuakeClient();
+        //client.createCSV();
+        //assertError("app should have a list of earthquake", client.createCSV());
+        // App classUnderTest = new App();
+        // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}   
+
+
+
+
